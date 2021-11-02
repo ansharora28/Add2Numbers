ab6e157388a966e753b1fc2877e48709fc3ae0f3
ansharora28 <ansh49ambala@gmail.com>
2021-11-02 09:03:48 +0530

    Added video

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function add() {
     document.getElementById("answer").value = a + b
     document.getElementById("ans").style.display = "block"
 }
+<script>
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+</script>
